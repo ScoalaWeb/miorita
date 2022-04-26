0a115cce0e22e0d6fcc6bad27d01e0acc6031594
vladutcornel <vladutcornel@users.noreply.github.com>
2022-04-26 18:53:32 +0000

    deploy: 6a15a84103153dc92078d7117ef7d5016e7fe236

--- _nuxt/static/1650997610/playgrounds/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1650997610",layout:"default",error:a,state:{code:""},serverRendered:true,routePath:"\u002Fplaygrounds",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"81c985b7"}}}}(null));
