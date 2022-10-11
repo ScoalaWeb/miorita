1a7a69df2fae913297953cfd38bf38942a5a51ef
vladutcornel <vladutcornel@users.noreply.github.com>
2022-10-11 20:10:08 +0000

    deploy: a64ad09186baa9ceb95e685723d2c9f920169277

--- _nuxt/static/1665518551/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1665518551",layout:"default",error:a,state:{code:""},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8d7ceeda"}}}}(null,"\u002F"));
---
+++ _nuxt/static/1665519006/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1665519006",layout:"default",error:a,state:{code:""},serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2b3bc1a7"}}}}(null,"\u002F"));
