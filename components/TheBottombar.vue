266f03df6f2c7cea2842b9ffe80ff13c9db8ee69
cezarin <cezarin@programminggurus.ro>
2022-11-07 23:20:17 +0200

    feat: [T1-20] updated scenario section

---
+++ components/TheBottombar.vue
@@ -0,0 +1,68 @@
+<template>
+    <div :class="$style.bottom">
+        <div :class="$style.leftSide">
+            <p :class="$style.slides">0/3</p>
+            <h3 :class="$style.brand">Meet Miorița</h3>
+        </div>
+        <div :class="$style.rightSide">
+            <button
+                :class="$style.button"
+            >
+                Back
+            </button>
+            <button
+                :class="$style.button"
+            >
+                Next
+            </button>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped module>
+    .bottom {
+        background-color: var(--background-color);
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+        justify-content: space-between;
+        height: 3rem;
+        width: 100%;
+    }
+
+    .leftSide {
+        display: flex;
+        flex-direction: row;
+        margin-left: 1rem;
+        align-items: center;
+        color: var(--text-color);
+    }
+
+    .slides {
+        margin-right: 0.5rem;
+        font-size: 1.5rem;
+    }
+
+    .brand {
+        font-family: CabinSketch, sans-serif;
+        font-size: 1.25rem;
+    }
+
+    .rightSide {
+        margin-right: 1rem;
+    }
+
+    .button {
+        width: 6.18rem;
+        height: 2rem;
+        background-color: var(--background-color);
+        border: 2px solid var(--color-gray-300);;
+        border-radius: 1.25rem;
+        font-size: 1.25rem;
+        color: var(--text-color);
+    }
+
+    .button:hover {
+        background-color: var(--secondary-button-hover);
+    }
+</style>
--- components/TheLesson.vue
+++ components/TheLesson.vue
@@ -1,17 +1,25 @@
 <template>
-    <div :class="$style.container">
-        <TheEditor :code="code" :class="$style.editor" />
-        <TheWorld :options="options" :class="$style.world">
-            <template #options>
-                <slot name="options" />
-            </template>
-        </TheWorld>
+    <div :class="$style.mainContainer">
+        <TheHeadbar :class="$style.headbar" />
+        <div :class="$style.container">
+            <TheDescription />
+            <TheEditor :code="code" :class="$style.editor" />
+            <TheWorld :options="options" :class="$style.world">
+                <template #options>
+                    <slot name="options" />
+                </template>
+            </TheWorld>
+        </div>
+        <TheBottombar />
     </div>
 </template>
 
 <script lang="ts">
 import { Vue, Component, Prop } from "vue-property-decorator";
+import TheBottombar from "~/components/TheBottombar.vue";
+import TheDescription from "~/components/TheDescription.vue";
 import TheEditor from "~/components/TheEditor.vue";
+import TheHeadbar from "~/components/TheHeadbar.vue";
 import TheWorld from "~/components/TheWorld.vue";
 import WorldOptions from "~/interfaces/WorldOptions";
 
@@ -19,8 +27,12 @@ import WorldOptions from "~/interfaces/WorldOptions";
     components: {
         TheWorld,
         TheEditor,
+        TheDescription,
+        TheHeadbar,
+        TheBottombar,
     },
 })
+
 export default class TheLesson extends Vue {
     @Prop(String) code!:string;
     @Prop(Object) options!:WorldOptions;
@@ -28,18 +40,29 @@ export default class TheLesson extends Vue {
 </script>
 
 <style module>
+.mainContainer {
+    height: 100vh;
+    width: 100vw;
+    display: grid;
+    grid: 5rem auto 3rem/ auto;
+}
+
+.headbar {
+    width: 100%;
+}
+
 .container {
     display: flex;
-    position: fixed;
     inset: 0;
 }
 
 .editor {
-    flex-basis: 50%;
+    flex-basis: 65%;
 }
 
 .world {
-    flex-basis: 50%;
+    flex-basis: 35%;
+    border: 1px solid var(--color-gray-500);
 }
 
 @media (orientation: portrait) {
