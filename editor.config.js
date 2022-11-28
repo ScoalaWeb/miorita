e8d05532f6c10c9c082dd3cd3dde9eda6b4a81b1
cezarin <cezarin@programminggurus.ro>
2022-11-28 23:31:41 +0200

    feat: [T1-45] added level editor configuration files

---
+++ editor.config.js
@@ -0,0 +1,71 @@
+export default {
+    // Target: https://go.nuxtjs.dev/config-target
+    target: "server",
+
+    dir: {
+        pages: "editor",
+    },
+
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: "Miorița level editor",
+        meta: [
+            { charset: "utf-8" },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1",
+            },
+        ],
+        link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [],
+
+    // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
+    plugins: [],
+
+    // Auto import components: https://go.nuxtjs.dev/config-components
+    components: true,
+
+    // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
+    buildModules: [
+        // https://go.nuxtjs.dev/typescript
+        "@nuxt/typescript-build",
+        // https://go.nuxtjs.dev/eslint
+        "@nuxtjs/eslint-module",
+        // https://go.nuxtjs.dev/stylelint
+        "@nuxtjs/stylelint-module",
+        // https://composition-api.nuxtjs.org/
+        "@nuxtjs/composition-api/module",
+        // https://github.com/nuxt-community/svg-module
+        "@nuxtjs/svg",
+        // https://pinia.vuejs.org/ssr/nuxt.html#nuxt-2-without-bridge
+        ["@pinia/nuxt", { disableVuex: false }],
+    ],
+
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+        // https://go.nuxtjs.dev/pwa
+        "@nuxtjs/pwa",
+        // https://go.nuxtjs.dev/content
+        "@nuxt/content",
+    ],
+
+    // PWA module configuration: https://go.nuxtjs.dev/pwa
+    pwa: {
+        manifest: {
+            lang: "en",
+        },
+    },
+
+    // Content module configuration: https://go.nuxtjs.dev/config-content
+    content: {
+        liveEdit: false,
+    },
+
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {
+        postcss: null,
+    },
+};
