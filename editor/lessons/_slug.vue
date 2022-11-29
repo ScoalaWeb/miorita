dcbf6c299f7e8ccfbb18112ac4f67ee22ade1e7e
Nico <nico@programminggurus.ro>
2022-11-29 18:12:05 +0200

    feat: [T1-54] added layout for editor

---
+++ editor/lessons/_slug.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>Lesson n</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    layout: "editor",
+};
+
+</script>
+
+<style lang="sass" scoped>
+
+</style>
