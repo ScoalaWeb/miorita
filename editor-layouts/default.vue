493d357f5b529fd1f96236599c690a46376b87b9
Nico <nico@programminggurus.ro>
2022-11-29 18:12:05 +0200

    feat: [T1-54] added layout for editor

---
+++ editor-layouts/default.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="editor_layout">
+        <nav class="editor_menu">
+            <NuxtLink class="editor_title" to="/">Miorița Editor</NuxtLink>
+            <div class="menu_items">
+                <NuxtLink to="/languages">Languages</NuxtLink>
+                <NuxtLink to="/texts">Texts</NuxtLink>
+                <NuxtLink to="/lessons">Lessons</NuxtLink>
+                <NuxtLink to="/playgrounds">Playgrounds</NuxtLink>
+            </div>
+        </nav>
+        <div class="editor_main">
+            <Nuxt />
+        </div>
+    </div>
+</template>
+
+<style lang="scss">
+@import "~/assets/fonts/Montserrat/Montserrat.css";
+
+html {
+    --font-editor: "Montserrat", sans serif;
+
+    font-family: "Source Sans Pro", -apple-system, BlinkMacSystemFont,
+        "Segoe UI", Roboto, "Helvetica Neue", Arial, sans-serif;
+    font-size: 16px;
+    word-spacing: 1px;
+    text-size-adjust: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+    background-color: var(--background-color);
+}
+
+*,
+*::before,
+*::after {
+    box-sizing: border-box;
+    margin: 0;
+}
+
+.editor_layout {
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 1rem;
+    padding-block: 1rem;
+    background-color: var(--color-gray-900);
+    color: var(--color-white);
+}
+
+.editor_menu {
+    width: 96%;
+    display: flex;
+    flex-flow: row wrap;
+    align-items: center;
+    justify-content: space-between;
+    border-radius: 1rem;
+    padding: 1rem 3rem ;
+    background-color: var(--color-gray-800);
+
+    a {
+        color: var(--color-white);
+        font-family: var(--font-editor);
+        font-size: 1.25rem;
+        font-weight: 500;
+        transition: all 100ms linear;
+
+        &.editor_title {
+            font-size: 1.75rem;
+            font-weight: 600;
+
+            &.nuxt-link-exact-active {
+                color: var(--color-white);
+            }
+        }
+
+        &:hover, &:focus, &:focus-within,
+        &.nuxt-link-exact-active {
+            color: var(--color-green-lighter);
+        }
+    }
+}
+
+.menu_items {
+    display: flex;
+    gap: 2rem;
+    flex-flow: row wrap;
+}
+
+.editor_main {
+    width: 96%;
+    height: 100%;
+    font-family: var(--font-editor);
+}
+
+</style>
--- editor/index.vue
+++ editor/index.vue
@@ -1,5 +1,44 @@
 <template>
-    <div>
-        <h1>This is a test of the level editor configuration.</h1>
-    </div>
+    <MainContainer>
+        <img
+            src="~/assets/img/character.svg"
+            alt="Miorița"
+            class="home_sheep"
+        >
+        <h1 class="home_title">It's time to edit Miorița</h1>
+        <NuxtLink class="home_button" to="languages">Get Started</NuxtLink>
+    </MainContainer>
 </template>
+
+<script setup>
+import MainContainer from "~/components/Editor/MainContainer";
+</script>
+
+<style lang="scss">
+@use "../assets/scss" as *;
+
+.home_sheep {
+    width: 10rem;
+    height: auto;
+}
+
+.home_title {
+    font-size: 3rem;
+}
+
+.home_button {
+    font-size: 1.25rem;
+    font-weight: 600;
+    padding: 0.5rem 2rem;
+    border-radius: 0.5rem;
+    background-color: var(--color-green);
+    color: var(--color-white);
+    transition: all 100ms linear;
+
+    &:hover, &:focus, &:focus-within {
+        background-color: var(--color-green-lighter);
+        color: var(--color-white);
+    }
+}
+
+</style>
