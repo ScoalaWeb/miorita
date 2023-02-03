39633da4df598c40de01c51fe775f05098cc3245
vladutcornel <vladutcornel@users.noreply.github.com>
2023-02-03 11:00:40 +0000

    deploy: 93571cb38ca6b3bd6e3da6876908b656f613ab15

--- _nuxt/static/1673951834/ro/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1673951834",layout:"default",error:a,serverRendered:true,routePath:"\u002Fro",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8ee6d704"}},globalRefs:{},pinia:{menu:{selected:"about",selectedIndex:0},scheme:{scheme:"",isLightScheme:false}}}}(null));
