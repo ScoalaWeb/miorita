13169d57ad377a421adb2664769790a4a3919d2b
vladutcornel <vladutcornel@users.noreply.github.com>
2023-04-21 17:11:00 +0000

    deploy: 647199e52a26da1bb9f762d6406b2847e5c959ee

--- _nuxt/static/1682096679/ro/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1682096679",layout:"default",error:a,serverRendered:true,routePath:"\u002Fro",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"2e104cd4"}},globalRefs:{},pinia:{menu:{selected:"about",selectedIndex:0},scheme:{scheme:"",isLightScheme:false}}}}(null));
---
+++ _nuxt/static/1682097059/ro/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1682097059",layout:"default",error:a,serverRendered:true,routePath:"\u002Fro",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"8c183788"}},globalRefs:{},pinia:{menu:{selected:"about",selectedIndex:0},scheme:{scheme:"",isLightScheme:false}}}}(null));
